a21d99301ff3b4bc6b207d51c12a882125b395f6
성철민 <tjdcjfals3@naver.com>
2024-02-05 17:46:52 +0900

    20240225 2231

---
+++ src/main/java/com/churnobyl/bj/bj20240225_2231.java
@@ -0,0 +1,34 @@
+package com.churnobyl.bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj20240225_2231 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		boolean flag = false;
+
+		for (int i = 0; i < N; i++) {
+			int total = i;
+			
+			int b = i;
+
+			while (b != 0) {
+				total += b % 10;
+				b /= 10;
+			}
+
+			if (total == N) {
+				System.out.println(i);
+				flag = true;
+				break;
+			}
+		}
+
+		if (!flag)
+			System.out.println(0);
+	}
+}
---
+++ src/test/java/com/churnobyl/bj/bj20240225_2231Test.java
@@ -0,0 +1,20 @@
+package com.churnobyl.bj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class bj20240225_2231Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
