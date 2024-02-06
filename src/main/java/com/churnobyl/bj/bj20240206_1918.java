6913ff459572cc932a808d43463f2e5dea48d5b8
Churnobyl <tjdcjfals@gmail.com>
2024-02-06 23:48:31 +0900

    20240206 1918

---
+++ src/main/java/com/churnobyl/bj/bj20240206_1918.java
@@ -0,0 +1,56 @@
+package com.churnobyl.bj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Stack;
+
+public class bj20240206_1918 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String text = br.readLine();
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for (int i = 0; i < text.length(); i++) {
+			char d = text.charAt(i);
+			
+			if (d == '*' || d == '/') {
+				while (!stack.isEmpty() && (stack.peek() == '*' || stack.peek() == '/')) {
+					bw.append(stack.pop());
+				}
+				
+				stack.push(d);
+			} else if (d == '+' || d == '-') {
+				while (!stack.isEmpty() && stack.peek() != '(') {
+					bw.append(stack.pop());
+				}
+				
+				stack.push(d);
+			} else if (d == '(') {
+				stack.push(d);
+			} else if (d == ')') {
+				while (!stack.isEmpty() && stack.peek() != '(') {
+					bw.append(stack.pop());
+				}
+				
+				if (!stack.isEmpty()) stack.pop();
+			} else {
+				bw.append(d);
+			}
+		}
+		
+		while (!stack.isEmpty())
+			bw.append(stack.pop());
+			
+		bw.flush();
+		bw.close();
+	}
+}
