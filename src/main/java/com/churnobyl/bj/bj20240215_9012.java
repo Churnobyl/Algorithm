96f8c36ccac558fa3c5ea70d6f46ae0e3e238db9
Churnobyl <tjdcjfals3@naver.com>
2024-02-15 15:37:14 +0900

    20240215 9012

--- src/main/java/com/churnobyl/bj/bj20240215_9012.java
+++ src/main/java/com/churnobyl/bj/bj20240215_9012.java
@@ -5,55 +5,92 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashMap;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class bj20240215_9012 {
-	static Stack<Character> stack = new Stack<>();
-	
+	static Stack<int[]> braStack = new Stack<>();
+	static Stack<int[]> valueStack = new Stack<>();	
+	static HashMap<Character, Character> map = new HashMap<>();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		char[] lines = br.readLine().toCharArray();
+
+		map.put(')', '(');
+		map.put(']', '[');
 		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int i = 0; i < T; i++) {
-			stack.clear();
+		int depth = 0;
+		boolean flag = true;
+
+		for (int i = 0; i < lines.length; i++) {
+			char data = lines[i];
 			
-			char[] lines = br.readLine().toCharArray();
-			boolean flag = true;
-			
-			for (int j = 0; j < lines.length; j++) {
-				if (stack.isEmpty()) {
-					if (lines[j] == ')') {
-						flag = false;
-						break;						
-					}
-					
-					stack.push(lines[j]);
-					continue;
+			if (data == '(' || data == '[') {
+				depth++;
+				braStack.push(new int[] {depth, data});
+			} else { // 닫는 경우
+				if (braStack.isEmpty()) {
+					// 비어있는 경우 말 안됨 0임
+					flag = false;
+					break;
 				}
 				
-				if (lines[j] == '(')
-					stack.push('(');
-				else {
-					if (stack.peek() == '(') {
-						stack.pop();
+				if (map.get(data) == braStack.peek()[1]) {
+					int[] braData = braStack.pop();
+					
+					if (valueStack.isEmpty()) {
+						if (braData[1] == '(')
+							valueStack.push(new int[] {braData[0], 2});
+						else if (braData[1] == '[')
+							valueStack.push(new int[] {braData[0], 3});
+					} else {
+						
+						int total = 0;
+						
+						while (!valueStack.isEmpty() && valueStack.peek()[0] > depth) {
+							total += valueStack.pop()[1];
+						}
+						
+						if (total == 0) {
+							if (braData[1] == '(')
+								valueStack.push(new int[] {braData[0], 2});
+							else if (braData[1] == '[')
+								valueStack.push(new int[] {braData[0], 3});
+						} else {
+							if (braData[1] == '(')
+								valueStack.push(new int[] {depth, total * 2});
+							else if (braData[1] == '[')
+								valueStack.push(new int[] {depth, total * 3});
+						}
 					}
+					
+				} else {
+					// 괄호 순서 다름 말 안됨
+					flag = false;
+					break;
 				}
+				
+				depth--;
 			}
-			
-			if (!flag || !stack.isEmpty())
-				bw.append("NO\n");
-			else
-				bw.append("YES\n");
 		}
 		
-		bw.flush();
+		if (braStack.isEmpty() && flag) {
+			int total = 0;
+			
+			while (!valueStack.isEmpty()) {
+				total += valueStack.pop()[1];
+			}
+			
+			bw.append(total + "");
+			bw.flush();
+		} else {
+			bw.append("0");
+		}
+		
+		
 		bw.close();
 	}
 }
