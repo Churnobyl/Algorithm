096cc7757d652c3efd3c49d8feed43caffba83a2
Churnobyl <tjdcjfals3@naver.com>
2024-02-22 17:01:47 +0900

    20240222_11726

---
+++ src/main/java/com/churnobyl/bj/bj20240221_11726.java
@@ -0,0 +1,25 @@
+package com.churnobyl.bj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class bj20240221_11726 {
+	static int n;
+	static int[] dp;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i < n + 1; i++) {
+			dp[i] = (dp[i - 1] % 10_007 + dp[i - 2] % 10_007) % 10_007;
+		}
+        
+        System.out.println(dp[n] % 10_007);
+    }
+}
