ea51ab2cad8a1c424c0d912802938e7fc6e63880
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2024-03-14 16:03:33 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 20328 ms, Memory: 243224 KB -BaekjoonHub

---
+++ Java/백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static int N;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i] + "\n");
+		}
+		System.out.println(sb);
+	}
+}
