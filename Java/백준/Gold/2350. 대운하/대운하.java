bf26bcb24c333c79401bcb866630448e32df10aa
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2024-03-31 19:33:56 +0900

    [Gold II] Title: 대운하, Time: 1172 ms, Memory: 48876 KB -BaekjoonHub

---
+++ Java/백준/Gold/2350. 대운하/대운하.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Edge implements Comparable<Edge> {
+    int s, e, w;
+
+    public Edge(int s, int e, int w) {
+        this.s = s;
+        this.e = e;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return o.w - this.w;
+    }
+}
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Edge[] edges = new Edge[M];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            edges[i] = new Edge(s, e, w);
+        }
+
+        Arrays.sort(edges);
+
+        parent = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            for (Edge edge : edges) {
+                union(edge.s, edge.e);
+                if (find(s) == find(e)) {
+                    System.out.println(edge.w);
+                    break;
+                }
+            }
+
+            for (int j = 0; j <= N; j++) {
+                parent[j] = j;
+            }
+        }
+    }
+
+    public static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+}
