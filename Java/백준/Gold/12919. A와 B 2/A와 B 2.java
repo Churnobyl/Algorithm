3dd59edf74a54c66bff62b60fdcf017e8a636d7b
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2024-04-03 11:08:09 +0900

    [Gold V] Title: A와 B 2, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- Java/백준/Gold/12919. A와 B 2/A와 B 2.java
+++ Java/백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -1,57 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Main {
+	static int Sa, Sb, Ta, Tb, Slen, now;
 	static String S, T;
-	static boolean isDab = false;
-	
+	Queue<int[]> queue = new LinkedList<>();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		S = br.readLine();
+		
+		for (int i = 0; i < S.length(); i++) {
+			if (S.charAt(i) == 'A') Sa++;
+			else Sb++;
+			
+			Slen++;
+		}
+		
 		T = br.readLine();
 		
-		dfs(T, true);
-		System.out.println(isDab ? 1 : 0);
-	}
-	
-	private static void dfs(String line, boolean isFront) {
-		if (line.length() == S.length()) {
-			if (isFront && line.equals(S))
-				isDab = true;
+		for (int i = 0; i < T.length(); i++) {
+			if (T.charAt(i) == 'A') Ta++;
+			else Tb++;
 			
-			if (!isFront) {
-				boolean check = true;
-				for (int i = 0; i < S.length(); i++) {
-					if (S.charAt(i) != line.charAt(S.length() - 1 - i)) {
-						check = false;
-						break;
-					}
-				}
-				
-				if (check) isDab = true;
-			}
-			
-			return;
+			now++;
 		}
 		
-		if (isFront) {
-			if (line.charAt(line.length() - 1) == 'A') {
-				dfs(line.substring(0, line.length() - 1), true);
+		if (recursion(T, true)) System.out.println(1);
+		else System.out.println(0);
+	}
+	
+	private static boolean recursion(String X, boolean drt) {
+		if (Ta < Sa || Tb < Sb) return false;
+		
+		if (Slen == now) {
+			if (S.equals(X) && drt || new StringBuffer(X).reverse().toString().equals(S) && !drt) {
+				return true;
 			}
-			
-			if (line.charAt(0) == 'B') {
-				dfs(line.substring(1, line.length()), false);
-			}
-		} else {
-			if (line.charAt(line.length() - 1) == 'B') {
-				dfs(line.substring(0, line.length() - 1), true);
-			}
-			
-			if (line.charAt(0) == 'A') {
-				dfs(line.substring(1, line.length()), false);
-			}					
+			else return false;
 		}
+		
+		if (drt) {
+			if (X.charAt(now - 1) == 'A') {
+				now--;
+				Ta--;
+				if (recursion(X.substring(0, now), drt)) return true;
+				now++;
+				Ta++;
+			}
+			
+			if (X.charAt(0) == 'B') {
+				now--;
+				Tb--;
+				if (recursion(X.substring(1, now + 1), false)) return true;
+				now++;
+				Tb++;
+			}
+		}
+		else {
+			if (X.charAt(0) == 'A') {
+				now--;
+				Ta--;
+				if (recursion(X.substring(1, now + 1), drt)) return true;
+				now++;
+				Ta++;
+			}
+			
+			if (X.charAt(now - 1) == 'B') {
+				now--;
+				Tb--;
+				if (recursion(X.substring(0, now), true)) return true;
+				now++;
+				Tb++;
+			}
+		}
+		
+		return false;
 	}
 }
