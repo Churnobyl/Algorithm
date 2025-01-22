f75ec81d9e65d169d1836b337ab9c8016c6d05ff
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-01-22 15:50:30 +0900

    [Gold I] Title: 외판원 순회, Time: 188 ms, Memory: 20236 KB -BaekjoonHub

--- Java/백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ Java/백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -5,18 +5,17 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * 외판원 순회
- * TSP 알고리즘
+ * 외판원 순회 - 동적 계획법
  */
 public class Main {
     static int N;
-    static int[][] costs;
+    static int[][] weight;
     static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        costs = new int[N][N];
+        weight = new int[N][N];
         dp = new int[N][1 << N];
         StringTokenizer st;
 
@@ -24,29 +23,33 @@ public class Main {
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
 
-        dp[0][1] = 0;
+        dp[0][1] = 0; // 0번째 도시에서 시작, 0번째 도시 방문처리하고 0으로 초기화
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < N; j++) {
-                costs[i][j] = Integer.parseInt(st.nextToken());
+                weight[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int bit = 0; bit < (1 << N); bit++) {
-            for (int curr = 0; curr < N; curr++) {
+        // 모든 방문지
+        for (int visited = 1; visited < (1 << N); visited++) {
+            for (int currentCity = 0; currentCity < N; currentCity++) {
                 // 현재 curr도시가 bit에 포함되어 있지않으면 패스
-                if ((bit & (1 << curr)) == 0) continue;
+                if ((visited & (1 << currentCity)) == 0) continue;
 
                 // 다음 도시 찾기
-                for (int next = 0; next < N; next++) {
-                    if ((bit & (1 << next)) != 0 || costs[curr][next] == 0) {
+                for (int nextCity = 0; nextCity < N; nextCity++) {
+                    // 이미 방문한 도시거나, 갈 수 있는 경로가 없을 경우 패스
+                    if ((visited & (1 << nextCity)) != 0 || weight[currentCity][nextCity] == 0) {
                         continue;
                     }
-                    if (dp[curr][bit] == Integer.MAX_VALUE) continue;
+                    //
+                    if (dp[currentCity][visited] == Integer.MAX_VALUE) continue;
 
-                    dp[next][(bit | (1 << next))] = Math.min(dp[next][bit | (1 << next)], dp[curr][bit] + costs[curr][next]);
+                    dp[nextCity][(visited | (1 << nextCity))] =
+                            Math.min(dp[nextCity][visited | (1 << nextCity)], dp[currentCity][visited] + weight[currentCity][nextCity]);
                 }
             }
         }
@@ -54,8 +57,8 @@ public class Main {
         int ans = Integer.MAX_VALUE;
 
         for (int i = 1; i < N; i++) {
-            if (costs[i][0] != 0 && dp[i][(1 << N) - 1] != Integer.MAX_VALUE) {
-                ans = Math.min(ans, dp[i][(1 << N) - 1] + costs[i][0]);
+            if (weight[i][0] != 0 && dp[i][(1 << N) - 1] != Integer.MAX_VALUE) {
+                ans = Math.min(ans, dp[i][(1 << N) - 1] + weight[i][0]);
             }
         }
 
