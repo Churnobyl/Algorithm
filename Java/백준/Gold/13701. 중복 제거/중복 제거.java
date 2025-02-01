cf2098bdc55e31a581f4061e4dc59edbd8dac7e9
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-02-01 10:14:23 +0900

    [Gold IV] Title: 중복 제거, Time: 1160 ms, Memory: 343932 KB -BaekjoonHub

--- Java/백준/Gold/13701. 중복 제거/중복 제거.java
+++ Java/백준/Gold/13701. 중복 제거/중복 제거.java
@@ -8,15 +8,12 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int MAX_VALUE = 33554432; // 2^25
-        int[] bitSet = new int[MAX_VALUE / 32]; // 비트 배열 (int형 배열 사용)
+        BitSet bitSet = new BitSet(MAX_VALUE);
 
         while (st.hasMoreTokens()) {
             int num = Integer.parseInt(st.nextToken());
-            int index = num / 32; // 해당 숫자가 저장될 int 배열의 인덱스
-            int bit = 1 << (num % 32); // 해당 숫자의 비트 위치
-
-            if ((bitSet[index] & bit) == 0) { // 아직 등장하지 않은 숫자라면
-                bitSet[index] |= bit; // 해당 비트를 켜기
+            if (!bitSet.get(num)) {
+                bitSet.set(num);
                 sb.append(num).append(" ");
             }
         }
