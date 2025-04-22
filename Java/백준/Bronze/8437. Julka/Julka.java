244eeb34613ed6e1c5834b2ab0bf259eb4204e31
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-04-22 15:05:00 +0900

    [Bronze V] Title: Julka, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ Java/백준/Bronze/8437. Julka/Julka.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+
+        System.out.println(a.add(b).divide(BigInteger.TWO));
+        System.out.println(a.subtract(b).divide(BigInteger.TWO));
+    }
+}
