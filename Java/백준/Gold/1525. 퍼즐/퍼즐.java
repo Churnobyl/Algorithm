9226e1aaf4fb0690cd5e9b9bc31a71afb9b1fa41
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-04-30 12:57:07 +0900

    [Gold II] Title: 퍼즐, Time: 632 ms, Memory: 80028 KB -BaekjoonHub

--- Java/백준/Gold/1525. 퍼즐/퍼즐.java
+++ Java/백준/Gold/1525. 퍼즐/퍼즐.java
@@ -3,108 +3,74 @@ import java.util.*;
 
 public class Main {
     static class State {
-        int[][] map;
-        int y;
-        int x;
+        String str;
         int count;
 
-        public State(int[][] map, int y, int x, int count) {
-            this.map = map;
-            this.y = y;
-            this.x = x;
+        public State(String str, int count) {
+            this.str = str;
             this.count = count;
         }
     }
 
-    static Set<String> cache = new HashSet<>();
     static int[] dy = {1, -1, 0, 0};
     static int[] dx = {0, 0, 1, -1};
-    static String answer = "123456780";
+    static final String GOAL = "123456780";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[][] map = new int[3][3];
-        int y = 0;
-        int x = 0;
+        StringBuilder start = new StringBuilder();
 
         for (int i = 0; i < 3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 0) {
-                    y = i;
-                    x = j;
-                }
+                start.append(st.nextToken());
             }
         }
 
-        String firstString = makeCache(map);
-        if (firstString.equals(answer)) {
+        String initial = start.toString();
+        if (initial.equals(GOAL)) {
             System.out.println(0);
             return;
         }
 
         Queue<State> queue = new ArrayDeque<>();
+        Set<String> visited = new HashSet<>();
 
-        queue.add(new State(map, y, x, 0));
+        queue.add(new State(initial, 0));
+        visited.add(initial);
 
-        boolean haveAnswer = false;
-
-        loop1:
         while (!queue.isEmpty()) {
-            State nextState = queue.poll();
+            State now = queue.poll();
+            String current = now.str;
+            int zeroIdx = current.indexOf('0');
+            int y = zeroIdx / 3;
+            int x = zeroIdx % 3;
 
-            for (int i = 0; i < 4; i++) {
-                int ny = nextState.y + dy[i];
-                int nx = nextState.x + dx[i];
+            for (int d = 0; d < 4; d++) {
+                int ny = y + dy[d];
+                int nx = x + dx[d];
 
-                if (0 <= ny && ny < 3 && 0 <= nx && nx < 3) {
-                    int[][] newMap = makeMap(nextState.map, nextState.y, nextState.x, ny, nx);
-                    String s = makeCache(newMap);
+                if (ny < 0 || ny >= 3 || nx < 0 || nx >= 3) continue;
 
-                    if (!cache.contains(s)) {
-                        if (s.equals(answer)) {
-                            haveAnswer = true;
-                            System.out.println(nextState.count + 1);
-                            break loop1;
-                        } else {
-                            cache.add(s);
-                            queue.add(new State(newMap, ny, nx, nextState.count + 1));
-                        }
+                int swapIdx = ny * 3 + nx;
+                char[] nextArr = current.toCharArray();
+
+                char temp = nextArr[zeroIdx];
+                nextArr[zeroIdx] = nextArr[swapIdx];
+                nextArr[swapIdx] = temp;
+
+                String next = new String(nextArr);
+                if (!visited.contains(next)) {
+                    if (next.equals(GOAL)) {
+                        System.out.println(now.count + 1);
+                        return;
                     }
+                    visited.add(next);
+                    queue.add(new State(next, now.count + 1));
                 }
             }
         }
 
-        if (!haveAnswer) {
-            System.out.println(-1);
-        }
-    }
-
-    private static String makeCache(int[][] newMap) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                sb.append(newMap[i][j]);
-            }
-        }
-
-        return sb.toString();
-    }
-
-    private static int[][] makeMap(int[][] map, int y, int x, int ny, int nx) {
-        int[][] newMap = new int[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            newMap[i] = map[i].clone();
-        }
-
-        int cache = newMap[y][x];
-        newMap[y][x] = newMap[ny][nx];
-        newMap[ny][nx] = cache;
-
-        return newMap;
+        System.out.println(-1);
     }
 }
