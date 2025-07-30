c693093db41b958b8bf1e9ec92035f83213ca0a0
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-07-30 11:24:53 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14444 KB -BaekjoonHub

---
+++ Java/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] map = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int[][][] dp = new int[N+1][N+1][3];
+        
+        dp[1][2][0] = 1;
+
+        for (int r = 1; r <= N; r++) {
+            for (int c = 2; c <= N; c++) {
+                if (map[r][c] == 0) {
+                    dp[r][c][0] += dp[r][c-1][0] + dp[r][c-1][2];
+                    dp[r][c][1] += dp[r-1][c][1] + dp[r-1][c][2];
+                    
+                    if (map[r-1][c] == 0 && map[r][c-1] == 0) {
+                        dp[r][c][2] += dp[r-1][c-1][0] + dp[r-1][c-1][1] + dp[r-1][c-1][2];
+                    }
+                }
+            }
+        }
+        
+        int answer = dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+        System.out.println(answer);
+    }
+}
