24d3400f3e11690252324494b6bfc5b4c40c363f
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-08-04 10:40:19 +0900

    [Silver I] Title: 카잉 달력, Time: 160 ms, Memory: 15236 KB -BaekjoonHub

--- Java/백준/Silver/6064. 카잉 달력/카잉 달력.java
+++ Java/백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -2,23 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int lcm(int a, int b) {
-        return a * b / gcd(a, b);
-    }
-
-    static int gcd(int a, int b) {
-        while (b != 0) {
-            int tmp = a % b;
-            a = b;
-            b = tmp;
-        }
-        return a;
-    }
+    static int T;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
+        T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         while (T-- > 0) {
@@ -28,20 +16,39 @@ public class Main {
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            int maxYear = lcm(M, N);
-            boolean found = false;
-
-            for (int k = x; k <= maxYear; k += M) {
-                if ((k % N == y % N)) {
-                    sb.append(k).append("\n");
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) sb.append(-1).append("\n");
+            sb.append(solve(M, N, x, y)).append("\n");
         }
 
         System.out.print(sb);
+    }
+    
+    static int[] extendedGCD(int a, int b) {
+        if (b == 0) return new int[]{a, 1, 0};
+        int[] vals = extendedGCD(b, a % b);
+        int g = vals[0];
+        int x = vals[2];
+        int y = vals[1] - (a / b) * vals[2];
+        return new int[]{g, x, y};
+    }
+
+    static int solve(int M, int N, int x, int y) {
+        int a = M;
+        int b = N;
+        int r = y - x;
+
+        int[] egcd = extendedGCD(a, b);
+        int g = egcd[0];
+        int s = egcd[1];
+        
+        if (r % g != 0) return -1;
+        
+        int nMod = b / g;
+        int t = ((r / g * s) % nMod + nMod) % nMod;
+        int k = a * t + x;
+        
+        int lcm = a / g * b;
+        if (k > lcm) return -1;
+
+        return k;
     }
 }
