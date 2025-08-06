8768ce87ae0c1253b826acb583f7e7dce9159c7a
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-08-06 11:37:08 +0900

    [Gold IV] Title: 비슷한 단어, Time: 316 ms, Memory: 38096 KB -BaekjoonHub

--- Java/백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ Java/백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -2,6 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        List<Integer> wordIndices = new ArrayList<>();
+    }
+
+    static class Result {
+        int maxLength = -1;
+        int bestSIndex = -1;
+        int bestTIndex = -1;
+        String word1 = "";
+        String word2 = "";
+
+        void updateWithOrder(int length, int sIndex, int tIndex, String[] words) {
+            if (length > maxLength ||
+                    (length == maxLength && sIndex < bestSIndex)) {
+                maxLength = length;
+                bestSIndex = sIndex;
+                bestTIndex = tIndex;
+                word1 = words[sIndex];
+                word2 = words[tIndex];
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,38 +36,39 @@ public class Main {
             words[i] = br.readLine();
         }
 
-        int maxPrefixLength = -1;
-        String resultS = "";
-        String resultT = "";
-
+        TrieNode root = new TrieNode();
+        Result result = new Result();
+        
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                int prefixLength = getCommonPrefixLength(words[i], words[j]);
-
-                if (prefixLength > maxPrefixLength) {
-                    maxPrefixLength = prefixLength;
-                    resultS = words[i];
-                    resultT = words[j];
-                }
-            }
+            insertAndFind(root, words[i], i, words, result);
         }
 
-        System.out.println(resultS);
-        System.out.println(resultT);
+        System.out.println(result.word1);
+        System.out.println(result.word2);
     }
 
-    private static int getCommonPrefixLength(String word1, String word2) {
-        int minLength = Math.min(word1.length(), word2.length());
-        int prefixLength = 0;
+    private static void insertAndFind(TrieNode root, String word, int wordIndex, String[] words, Result result) {
+        TrieNode current = root;
 
-        for (int i = 0; i < minLength; i++) {
-            if (word1.charAt(i) == word2.charAt(i)) {
-                prefixLength++;
-            } else {
-                break;
+        for (int depth = 0; depth < word.length(); depth++) {
+            int charIndex = word.charAt(depth) - 'a';
+
+            if (current.children[charIndex] == null) {
+                current.children[charIndex] = new TrieNode();
             }
-        }
 
-        return prefixLength;
+            current = current.children[charIndex];
+            
+            for (int existingIndex : current.wordIndices) {
+                int prefixLen = depth + 1;
+                if (existingIndex < wordIndex) {
+                    result.updateWithOrder(prefixLen, existingIndex, wordIndex, words);
+                } else {
+                    result.updateWithOrder(prefixLen, wordIndex, existingIndex, words);
+                }
+            }
+            
+            current.wordIndices.add(wordIndex);
+        }
     }
 }
