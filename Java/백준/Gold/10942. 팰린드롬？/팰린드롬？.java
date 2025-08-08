1f3e441440a3c60637278963d09305c6738746f3
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-08-08 13:02:08 +0900

    [Gold IV] Title: 팰린드롬?, Time: 700 ms, Memory: 218012 KB -BaekjoonHub

--- Java/백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ Java/백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,22 +1,26 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main {
     static int N, M;
     static int[] arr;
-    static Boolean[][] memo;
+    static boolean[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
-        memo = new Boolean[N][N];
+        dp = new boolean[N][N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        
+        precompute();
 
         M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
@@ -25,25 +29,33 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken()) - 1;
             int E = Integer.parseInt(st.nextToken()) - 1;
-            sb.append(isPalindrome(S, E) ? 1 : 0).append("\n");
+            sb.append(dp[S][E] ? 1 : 0).append("\n");
         }
 
         System.out.print(sb);
     }
 
-    private static boolean isPalindrome(int S, int E) {
-        if (memo[S][E] != null) {
-            return memo[S][E];
-        }
-        
-        if (S >= E) {
-            return memo[S][E] = true;
+    private static void precompute() {
+        // 길이 1
+        for (int i = 0; i < N; i++) {
+            dp[i][i] = true;
         }
 
-        if (arr[S] != arr[E]) {
-            return memo[S][E] = false;
+        // 길이 2
+        for (int i = 0; i < N - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                dp[i][i + 1] = true;
+            }
         }
-        
-        return memo[S][E] = isPalindrome(S + 1, E - 1);
+
+        // 길이 3 이상
+        for (int len = 3; len <= N; len++) {
+            for (int i = 0; i <= N - len; i++) {
+                int j = i + len - 1;
+                if (arr[i] == arr[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
     }
 }
