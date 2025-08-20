65dd5828bdd60c8ac57f49d7c24ade971736ad20
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-08-20 10:12:48 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1148 ms, Memory: 275908 KB -BaekjoonHub

---
+++ Java/백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, K, X;
+    static boolean[] cities;
+    static List<Integer>[] edges;
+    static int[] dp;
+    static int cnt;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        cities = new boolean[N + 1];
+        edges = new List[N + 1];
+        dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (edges[a] == null) edges[a] = new ArrayList<>();
+            edges[a].add(b);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+        pq.add(new int[] {0, X});
+        dp[X] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] nxt = pq.poll();
+            int dist = nxt[0];
+            int node = nxt[1];
+
+            if (dp[node] < dist) continue;
+
+            List<Integer> edge = edges[node];
+
+            if (edge == null) continue;
+
+            for (int e : edge) {
+                if (dp[e] > dist + 1) {
+                    if (dp[e] == K) {
+                        cities[e] = false;
+                        cnt--;
+                    }
+                    dp[e] = dist + 1;
+                    if (dp[e] == K) {
+                        cities[e] = true;
+                        cnt++;
+                    }
+                    pq.add(new int[] {dist + 1, e});
+                }
+            }
+        }
+
+        if (cnt == 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < N + 1; i++) {
+            if (cities[i]) sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
