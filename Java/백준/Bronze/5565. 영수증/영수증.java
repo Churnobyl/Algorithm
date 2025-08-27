f7aa0f822e220d06a5e5449d7d0800811903b9fc
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-08-28 08:41:57 +0900

    [Bronze IV] Title: 영수증, Time: 96 ms, Memory: 14212 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < 9; i++) {
+            N -= Integer.parseInt(br.readLine());
+        }
+        System.out.println(N);
+    }
+}
