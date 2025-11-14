469ea04dcf5d19688a2de35ff76f04ccac5c0cf6
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-11-14 15:07:44 +0900

    [Bronze III] Title: 공, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1547. 공/공.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int ball = 1;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == ball || b == ball) {
+                if (a == ball) ball = b;
+                else ball = a;
+            }
+        }
+
+        System.out.println(ball);
+    }
+}
