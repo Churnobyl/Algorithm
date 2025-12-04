0241131df8349115fc0b26f3964e50ea7b963d31
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2025-12-04 17:14:10 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 112 ms, Memory: 14320 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0) sb.append(" ");
+
+            for (int j = 0; j < N; j++) {
+                sb.append("* ");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
