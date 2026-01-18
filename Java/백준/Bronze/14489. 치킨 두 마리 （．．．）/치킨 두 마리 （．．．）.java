9e6cfc8f79c219f1d98e0ca1a7d6647a49ccb79a
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2026-01-18 14:08:14 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine());
+
+        int sum = A + B;
+        if (sum >= 2 * C) System.out.println(sum - 2 * C);
+        else System.out.println(sum);
+    }
+}
