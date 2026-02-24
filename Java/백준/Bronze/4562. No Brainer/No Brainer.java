c9ee66f6803398901f1927809fe00b0ca11de33a
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2026-02-25 00:03:09 +0900

    [Bronze IV] Title: No Brainer, Time: 100 ms, Memory: 13964 KB -BaekjoonHub

---
+++ Java/백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+
+            if (X >= Y) sb.append("MMM BRAINS");
+            else sb.append("NO BRAINS");
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
