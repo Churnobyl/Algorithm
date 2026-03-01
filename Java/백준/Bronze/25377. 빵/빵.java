def0af93c0bf9ab30fd85019bb9492e1665b5351
Churnobyl <77923267+Churnobyl@users.noreply.github.com>
2026-03-01 12:05:27 +0900

    [Bronze IV] Title: 빵, Time: 108 ms, Memory: 14432 KB, Score: 100 point -BaekjoonHub

---
+++ Java/백준/Bronze/25377. 빵/빵.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A <= B) min = Math.min(min, B);
+        }
+
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+    }
+}
